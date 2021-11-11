38ed92644a3aff5370cc286a900ac15c39a606bb
Elisey Ekhlakov <ekhalakovea@bk.ru>
2021-11-11 23:58:50 +0400

    +dataBinding observe ViewModel

--- app/src/main/java/ru/adorly/adcomposition/presentation/BindingAdapters.kt
+++ app/src/main/java/ru/adorly/adcomposition/presentation/BindingAdapters.kt
@@ -1,10 +1,18 @@
 package ru.adorly.adcomposition.presentation
 
+import android.content.Context
+import android.content.res.ColorStateList
 import android.widget.ImageView
+import android.widget.ProgressBar
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.databinding.BindingAdapter
 import ru.adorly.adcomposition.R
 import ru.adorly.adcomposition.domain.entity.GameResult
+
+interface OnOptionClickListener {
+    fun onOptionClick(option: Int)
+}
 
 @BindingAdapter("requiredAnswers")
 fun bindRequiredAnswers(textView: TextView, count: Int) {
@@ -46,7 +54,7 @@ private fun getPercentOfRightAnswers(gameResult: GameResult) = with(gameResult) 
 }
 
 @BindingAdapter("resultEmoji")
-fun bindResultEmoji(imageView: ImageView, winner: Boolean){
+fun bindResultEmoji(imageView: ImageView, winner: Boolean) {
     imageView.setImageResource(getSmileResId(winner))
 }
 
@@ -55,5 +63,37 @@ private fun getSmileResId(winner: Boolean): Int {
         R.drawable.ic_smile
     } else {
         R.drawable.ic_sad
+    }
+}
+
+@BindingAdapter("enoughCount")
+fun bindEnoughCount(textView: TextView, enough: Boolean) {
+    textView.setTextColor(getColorByState(textView.context, enough))
+}
+
+@BindingAdapter("enoughPercent")
+fun bindEnoughPercent(progressBar: ProgressBar, enough: Boolean) {
+    val color = getColorByState(progressBar.context, enough)
+    progressBar.progressTintList = ColorStateList.valueOf(color)
+}
+
+private fun getColorByState(context: Context, goodSate: Boolean): Int {
+    val colorResId = if (goodSate) {
+        android.R.color.holo_green_light
+    } else {
+        android.R.color.holo_red_light
+    }
+    return ContextCompat.getColor(context, colorResId)
+}
+
+@BindingAdapter("numberAsText")
+fun bindNumberAsText(textView: TextView, number: Int) {
+    textView.text = number.toString()
+}
+
+@BindingAdapter("onOptionClickListener")
+fun bindOnOptionClickListener(textView: TextView, clickListener: OnOptionClickListener) {
+    textView.setOnClickListener {
+        clickListener.onOptionClick(textView.text.toString().toInt())
     }
 }
